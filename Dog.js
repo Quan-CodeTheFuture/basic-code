34481a0208f2f61a7c92c949ef5a330d2fdd5994
Quan Bao <quy123bao20@gmail.com>
2020-04-11 16:25:05 +0700

    add new skill: dog had ability to die

--- Dog.js
+++ Dog.js
@@ -1,8 +1,17 @@
 function Dog(name){
     this.name = name;
     this.stomach = [];
+    this.death=false;
 }
 
-prototype.Dog.eat = function(cat){
+Dog.prototype.eat = function(cat){
     this.stomach.push(cat)
 }
+
+Dog.prototype.die = function(){
+    this.death = true;
+}
+
+var dog = new Dog('Milu');
+dog.die();
+console.log(dog.death);
