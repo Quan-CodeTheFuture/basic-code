97c7d9c47fb5948037a34d936be51864cc67d35f
Quan Bao <quy123bao20@gmail.com>
2020-04-11 22:33:53 +0700

    Change the length of grid

---
+++ linkedList.js
@@ -0,0 +1,30 @@
+function Spot(i,grid){
+    this.i = i;
+    this.value = grid[i];
+    this.neightbors = [];
+    this.savePrevious=function(){
+        if(i-1 >= 0){
+            this.neightbors.push(grid[i-1]);
+        }
+        else{
+            this.neightbors.push(undefined);
+        }
+    }
+}
+var grid = [9,3,5,6,9,10];
+for(var i = 0; i <= grid.length; i++){
+    grid[i] = new Spot(i,grid);
+}
+
+for(var i = 0; i <= grid.length; i++){
+    grid[i].savePrevious();
+}
+
+var i = grid.length;
+var temp = grid[i].neightbors[0];
+
+while(temp){
+    console.log(temp.value);
+    i-=1;
+    temp = grid[i].neightbors[0];
+}
